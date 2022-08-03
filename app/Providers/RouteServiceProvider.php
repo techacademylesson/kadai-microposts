4c6ade623b09c290b0e482f8525ca2931bad749f
techacademy.lesson <kthrowaway0001@gmail.com>
2022-08-03 07:05:07 +0000

    user registration

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';    // リダイレクト先をトップページへ
 
     /**
      * Define your route model bindings, pattern filters, etc.
