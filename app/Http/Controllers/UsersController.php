1423ce734a3351bd98f162a181f4cbed27b604ba
techacademy.lesson <kthrowaway0001@gmail.com>
2022-08-04 08:17:37 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,9 +23,16 @@ class UsersController extends Controller
         // idでユーザ検索して取得
         $user = User::findOrFail($id);
         
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+        
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+        
         // ユーザ詳細ビューを表示
         return view('users.show', [
             'user' => $user,
+            'microposts' => $microposts,
         ]);
     }
 }
